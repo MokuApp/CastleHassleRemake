aa2d831974678414179e51825cb1c02188770b9d
MokuApp <mokuappforgit@gmail.com>
2012-12-18 17:06:23 +0900

    Initial Commit

---
+++ CastleHassleRemake/HelloWorldLayer.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldLayer.h
+//  CastleHassleRemake
+//
+//  Created by tedant on 12/12/18.
+//  Copyright MokuApp 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+#import "Box2D.h"
+#import "GLES-Render.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+	b2World* world;
+	GLESDebugDraw *m_debugDraw;
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+// adds a new sprite at a given coordinate
+-(void) addNewSpriteWithCoords:(CGPoint)p;
+
+@end
---
+++ CastleHassleRemake/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CastleHassleRemake
+//
+//  Created by tedant on 12/12/18.
+//  Copyright MokuApp 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
